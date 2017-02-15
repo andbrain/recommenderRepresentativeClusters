ba17c0ff7ab4aa2561a9a8b17d4f96f0cf1dfe02
Anderson Pimentel <andbrain@gmail.com>
2017-02-15 13:55:45 -0400

    feat(clustering): python - add median function and remove unused files

--- clustering/python/ap.py
+++ clustering/python/ap.py
@@ -3,15 +3,30 @@ from scipy.sparse import coo_matrix
 from numpy import *
 import sys
 
-def readDataset():
+def getMedianValue(simValues):
+	# getting median value
+	dataSorted = sort(simValues)
+	size = len(dataSorted)
+	print "Tamanho: ", size
+
+	if(size % 2 == 0):
+		median = ( (dataSorted[size/2]) + (dataSorted[size/2 - 1]) )/2
+	else:
+		median = dataSorted[size/2]
+
+	return median
+
+def readSimMatrix():
 	i = array([])
 	j = array([])
 	data = array([])
+	counter = 0;
+	accum = 0;
 
-	print "[INFO] Constructing matrix.."
+	print "[INFO] Reading sim matrix.."
 	# with open("Similarities_toy_Sim.txt") as f:
 	# with open("Similarities.txt") as f:
-	with open("ml100k/Similarities.txt") as f:
+	with open("similarities.dat") as f:
 		content = f.readlines()
 
 	# content = [line.strip().split() for line in content]
@@ -20,34 +35,29 @@ def readDataset():
 		line = line.split()
 		i = append(i, int(line[0]) - 1)
 		j = append(j, int(line[1]) - 1)
-		data = append(data, float(line[2]))
+		value = float(line[2])
+		data = append(data, value)
+		accum += value;
+		counter += 1
+	
+	median = getMedianValue(data)
+	print "Preferences: ", median, " (median of similarities)"
 
-	return i,j,data
+	return i,j,data, median
 
 def main():
-	i,j,data = readDataset()
-	
-	# print i,j,data
-	simMatrix = coo_matrix((data,(i, j)))
+	i,j,data, median = readSimMatrix()
 
-	# print type(simMatrix), sys.getsizeof(simMatrix)
-	# print simMatrix.toarray()
-	print simMatrix
+	simMatrix = coo_matrix((data,(i, j)))
 
 	# af = AffinityPropagation(damping=0.9, preferences=0.014035).fit(simMatrix)
 	# af = AffinityPropagation(damping=0.9, preference=0.014035, affinity='precomputed').fit(simMatrix.todense())
-
 	# af = AffinityPropagation(damping=0.9, preference=0.014035, affinity='precomputed').fit(simMatrix.toarray())
-	af = AffinityPropagation(damping=0.9, preference=0.014035,affinity='precomputed').fit(simMatrix.toarray())
-	cluster_centers_indices = af.cluster_centers_indices_
-	labels = af.labels_
-
-	n_clusters_ = len(cluster_centers_indices)
+	af = AffinityPropagation(damping=0.9, preference=median,affinity='precomputed').fit(simMatrix.toarray())
 
 	print('Clusters indices: ', af.cluster_centers_indices_)
-	# print('Clusters centers: ', af.cluster_centers_.toarray())
 	print('Clusters labels: ', af.labels_)
-	print('Estimated number of clusters: %d' % n_clusters_)
+	print('Estimated number of clusters: %d' % len(af.cluster_centers_indices_))
 
 if __name__ == '__main__':
 	main()
