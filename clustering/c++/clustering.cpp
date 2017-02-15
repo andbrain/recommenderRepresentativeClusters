e52bc73ece7b40d16401bc55ec6130000dbd5c5f
Anderson Pimentel <andbrain@gmail.com>
2017-02-15 02:10:18 -0400

    chore(c++_ap): fix gitignore

--- clustering/c++/clustering.cpp
+++ clustering/c++/clustering.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
-
-#include "include/parser.h"
+#include "include/graph.h"
+#include "include/ap.h"
 
 using namespace std;
 
 string sim_matrix_path;
 string ratings_path;
+int iteration;
+double dampingFactor;
 
 void GetDatasetName(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
 	GetDatasetName(argc, argv);
+
+	// Graph *graph = p->GetSimMatrix();
+	// Graph *sim = p->GetSimMatrix();
+
+	// AP *ap = new AP(iteration, dampingFactor);
+	// ap->SetSimMatrix(graph);
+	// ap->SetRatings(ratings);
+	// ap->Process();
+	
+	// delete ap;
 	
 	return 0;
 }
 
 void GetDatasetName(int argc, char *argv[])
 {
-	if(argc == 3)
+	if(argc == 5)
 	{
 		sim_matrix_path = argv[1];
 		ratings_path = argv[2];
+		iteration = atoi(argv[3]);
+		dampingFactor = stod(argv[4]);
 	}
 	else
 	{
 		cout << "[Error] Missing parameters!" << endl;
-		cout << "[INFO] ./clustering.out SIM_MATRIX_PATH RATINGS_PATH" << endl;
+		cout << "[INFO] ./ap.out SIM_MATRIX_PATH RATINGS_PATH ITERATIONS DAMPING_FACTOR" << endl;
 		exit(1);
 	}
 }
---
+++ clustering/c++/include/ap.h
@@ -0,0 +1,45 @@
+#ifndef AP_H
+#define AP_H
+
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <cmath>
+#include <algorithm>
+#include "graph.h"
+
+using namespace std;
+
+struct PairValue
+{
+	double index;
+	int freq;
+};
+	
+class AP
+{
+public:
+	AP(int iterations, double lambda);
+	~AP();
+	void SetSimMatrix(Graph* graph);
+	void SetRatings(Graph* graph);
+	void Process();
+private:
+	Graph *mS, *mR, *mA, *mE, *mRatings; 
+	int mIter;
+	double mLambda;
+	void AffinityPropagation();
+	void PrintCluster();
+	void OutputClusters(map<int,vector<int>> *clusters);
+	void UpdateResponsability();
+	void UpdateAvailability();
+	
+	void CreateTrainFile(int *users, int size, map<int,vector<int>> *clusters, double percTrain);
+	
+	map<int, map<int, vector<double>> > AccumulateRatings(map<int,vector<int>> *clusters);
+	void CalculateRepresentative(map<int,vector<int>> *clusters);
+	void MakeRepresentativeByFrequency(map<int, map<int, vector<double>>> *accumList);
+	void MakeRepresentativeByMean(map<int, map<int, vector<double>>> *accumList, map<int,vector<int>> *clusters);
+};
+
+#endif
